fa4f92173d46ced7b86bafd231d92ee3f92704f2
Deepu <Deepu@DESKTOP-VA5DMJC>
2019-02-15 00:07:18 -0600

    Enabling Service registry using eureka server and test load balanced

--- src/main/java/com/deepak/ratingsdataservice/controller/RatingsDataController.java
+++ src/main/java/com/deepak/ratingsdataservice/controller/RatingsDataController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,9 @@ import com.deepak.ratingsdataservice.model.Rating;
 @RequestMapping("/ratings")
 public class RatingsDataController
 {
+   @Value("${eureka.instance.instance-id}")
+   private String instanceId;
+   
    @RequestMapping( method=RequestMethod.GET, path="/movies/{movieId}")
    public Rating getRatingForMovie( @PathVariable("movieId") String movieId )
    {
@@ -24,6 +28,8 @@ public class RatingsDataController
    @RequestMapping( method=RequestMethod.GET, path="/users/{userId}")
    public List< Rating > getRatingsForUser( @PathVariable("userId") String userId )
    {
+      System.out.println( "Instance Id for ratings-data-service : " + instanceId );
+      
       return Stream.< Rating > builder()
                    .add( new Rating( "1234", 4 ) )
                    .add( new Rating( "1236", 6 ) )
